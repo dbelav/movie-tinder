70e482fc17d4c9ee41915e0b5e783469551986a4
someonecoding <someone.coding@gmail.com>
2023-11-06 16:22:12 +0200

    [HOTFIX] all trusted hosts

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
+origins = [
+    "*"
+]
 
 app.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["*"]
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 @app.get("/trending/")
 def trending():
     return [
