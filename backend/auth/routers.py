acae2b7e0ee9c49fe486fcec0413f072b4b52819
someonecoding <someone.coding@gmail.com>
2023-11-30 01:01:27 +0200

    [HOTFIX]

--- backend/auth/routers.py
+++ backend/auth/routers.py
@@ -12,10 +12,11 @@ user_crud = UserCRUD()
 
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
-def register(username: str, password: str, response: Response):
-    result = user_crud.create_user(username, password)
+def register(user: UserSchema, response: Response):
+    result = user_crud.create_user(user.username, user.password)
     if not result["status"]:
         response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"message": result["message"]}
     return {
         "message": result["message"],
         "user_id": result["instance"].id,
